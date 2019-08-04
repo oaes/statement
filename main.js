f1ef9926423b8fda3be3f65426378286f0517e4b
Oaes Kuruni <oaeskuruni27@gmail.com>
2019-08-04 23:45:59 +0600

    added a file

---
+++ main.js
@@ -0,0 +1,20 @@
+//continue statement in  js......or skipped
+
+
+
+// for (var n = 0; n < 10; n++) {
+//     if (n == 3 || n == 9) {
+//         continue
+//     } else (
+//         console.log(n)
+//     )
+// }
+
+
+for (var x = 8; x < 20; x++) {
+    if (x == 12 || x == 16) {
+        continue
+    } else {
+        console.log(x);
+    }
+}
